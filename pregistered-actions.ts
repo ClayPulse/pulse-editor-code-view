16be3a23b3b0c7db19a7232e4695b02be1e29c16
Shellishack <github-action@actions-template-sync.noreply.github.com>
2025-11-09 02:06:32 +0000

    chore(template): merge template changes :up:
    Signed-off-by: Shellishack <github-action@actions-template-sync.noreply.github.com>

--- pregistered-actions.ts
+++ pregistered-actions.ts
@@ -1,45 +1,14 @@
 import { Action } from "@pulse-editor/shared-utils";
 
 export const preRegisteredActions: Record<string, Action> = {
-  "coding-using-file-content": {
-    name: "Coding Using File Content",
-    description: "Generate or modify code using file content",
-    parameters: {
-      userMessage: {
-        type: "string",
-        description: "The message to send to the agent.",
-      },
-      knowledge: {
-        type: "string",
-        description:
-          "Additional knowledge that might be useful for the agent to understand user's instruction. \
-This is to give the agent more context about the task so they don't hallucinate. ",
-      },
-    },
+  "example-action": {
+    name: "Example action",
+    description: "This is an example action.",
+    parameters: {},
     returns: {
       response: {
         type: "string",
         description: "The result of the example action.",
-      },
-    },
-  },
-  "vibe-code-pulse-app": {
-    name: "Vibe Code Pulse App",
-    description: "Develop a Pulse App with vibe coding",
-    parameters: {
-      userMessage: {
-        type: "string",
-        description: "The user message to send to the agent.",
-      },
-      terminal: {
-        type: "app-instance",
-        description: "The Pulse App terminal instance to run commands in.",
-      },
-    },
-    returns: {
-      response: {
-        type: "string",
-        description: "Agent's response to the user message.",
       },
     },
   },
